49ffbf5b1946f7e80b7948ddce8318518c8d0e7b
shathaalharmi <alharmi2003s@gmail.com>
2026-04-06 15:09:36 +0400

    add

---
+++ 46.py
@@ -0,0 +1,17 @@
+inputStr = input("Enter number or empty string to stop: ")
+negativeCount = 0
+while inputStr !='':
+    
+    value = int(inputStr)
+    
+    if value < 0: 
+        negativeCount = negativeCount + 1
+            print("That wasn't a whole number. Try again!")
+        inputStr = input("Enter number or empty string to stop: ")
+print(negativeCount)
+
+
+        
+        
+    
+        
---
+++ shatha assig 54.py
@@ -0,0 +1,25 @@
+balance = 1000
+username = input("Enter username: ")
+password = int(input("Enter password: "))
+if username == "shatha"and password == 1234 :
+    print("access granted , select one option")
+    print("1.check balance")
+    print("2.deposit money")
+    print("3.withdraw money")
+    
+selectOption =input("Enter option: 1,2,3: ")
+
+  if selectOption == "1" :
+     print("your current amount is 1000")
+  elif selectOption == "2" :
+    newBalance = float(input("Enter amout to deposit: "))
+     print("successfully deposited, you current amount is",balance + newBalance)
+  elif selectOption == "3" :
+    newBalance = float(input("Enter amout to withdraw: "))
+  if newBalance <= balance :
+     print("successfully withdraw, you current amount is",balance - newBalance)
+  else :
+     print("no enough amount")
+     
+else :
+    print("access denaid") 
