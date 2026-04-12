90904d79c82f9da39d02e5f733f4be21ff8ea522
shathaalharmi <alharmi2003s@gmail.com>
2026-04-12 08:41:57 +0400

    add

---
+++ bank amount using def.py
@@ -0,0 +1,48 @@
+balance = 1000
+def login(username, password):
+    return username == "shatha" and password == 1234
+
+
+def deposit():
+    global balance
+    balance = balance + amount
+
+
+def withdraw():
+    global balance
+    if amount <= balance:
+      balance = balance - amount
+    else:
+        print("Not enough amount.")
+        return balance
+
+
+username = input("Enter username: ")
+password = int(input("Enter password: "))
+while True :
+    if username == "shatha"and password == 1234 :
+      print("access granted , select one option")
+      print("1.check balance")
+      print("2.deposit money")
+      print("3.withdraw money")
+      choice = input("Enter option (1, 2, 3): ")
+      if choice == "1":
+             print("Your current amount is:", balance)
+
+      elif choice == "2":
+            amount = float(input("Enter amount to deposit: "))
+            balance = deposit()
+            print("successfully deposited, you current amount is", balance)
+
+      elif choice == "3":
+            amount = float(input("Enter amount to withdraw: "))
+            balance = withdraw()
+            print("successfully withdraw, you current amount is:", balance)
+
+      else:
+            print("no enough amount")
+
+else:
+    print("Access denied")
+    
+    
